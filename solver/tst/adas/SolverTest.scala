512ad3113ea0b5728a430aa862f6d8d8b6594378
John Gorski <john.gorski.jr@gmail.com>
2014-11-11 18:24:50 -0600

    Generic Scala solver sans tests

---
+++ solver/tst/adas/SolverTest.scala
@@ -0,0 +1,13 @@
+package adas
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SolverTest extends FunSuite {
+  test("one plus one is two") {
+    assert(1 + 1 === 2)
+  }
+}
