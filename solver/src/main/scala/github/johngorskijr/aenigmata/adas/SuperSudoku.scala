71253868f1a8ff21e9522908d57e719eb3e00884
John Gorski <john.gorski.jr@gmail.com>
2015-03-22 17:23:26 -0700

    Progress up to consulting hints

--- solver/src/main/scala/github/johngorskijr/aenigmata/adas/SuperSudoku.scala
+++ solver/src/main/scala/github/johngorskijr/aenigmata/adas/SuperSudoku.scala
@@ -1,5 +1,7 @@
 package github.johngorskijr.aenigmata.adas
 
+import scala.util.{Failure, Success, Try}
+
 object SuperSudoku {
   type Cell = Set[Int]
 
@@ -37,9 +39,9 @@ object SuperSudoku {
     } yield Location(rowIndex, colIndex) + ": " + cellToString(cellAt(Location(rowIndex, colIndex)))
       ).mkString("\n")
 
-    def row(index: Int) = state(index)
+    def row(index: Int): Vector[Cell] = state(index)
 
-    def col(index: Int) = indices.map(state(_)(index))
+    def col(index: Int): Vector[Cell] = indices.map(state(_)(index)).toVector
 
     def withCell(location: Location, cell: Cell): Puzzle = location match {
       case Location(row, col) =>
@@ -49,6 +51,11 @@ object SuperSudoku {
 
     def withCell(location: Location, cellTransform: Cell => Cell): Puzzle =
       withCell(location, cellTransform(cellAt(location)))
+    
+    def withRow(rowIndex: Int)(row: Vector[Cell]): Puzzle = Puzzle(state.updated(rowIndex, row), numericConstraints, ltConstraints)
+    
+    def withCol(colIndex: Int)(col: Vector[Cell]): Puzzle =
+      indices.foldLeft(this)((p, rowIndex) => p.withRow(rowIndex)(p.row(rowIndex).updated(colIndex, col(rowIndex))))
 
     lazy val size = state.length
     lazy val indices = 0 until size
@@ -129,5 +136,116 @@ object SuperSudoku {
     pigeons.foldLeft(p)((puz: Puzzle, pigeon: (Location, Int)) => pigeon match {
       case (l, v) => puz.set(l, v)
     })
+  }
+
+  // if there are two cells in a row or column that are each limited to one of two values, remove those values from
+  // consideration in the other rows or columns
+  def rayHeuristic(extractRay: Puzzle => Vector[Cell], replaceRay: (Puzzle, Vector[Cell]) => Puzzle): Puzzle => Puzzle = p => {
+    val ray = extractRay(p)
+    replaceRay(p, updateRay(ray))
+  }
+
+  // if there are two cells in a ray that are each limited to one of two values, remove those values from
+  // consideration in the other rays
+  def updateRay(ray: Vector[Cell]): Vector[Cell] = {
+    val pairs = for {
+      i <- 0 until (ray.size - 1)
+      if ray(i).size == 2
+      j <- (i + 1) until ray.size
+      if ray(i) == ray(j)
+    } yield (i, j, ray(i))
+
+    pairs.foldLeft(ray)((cells: Vector[Cell], pair: (Int, Int, Cell)) => pair match {
+      case (n, m, c) => cells.map(cell => cell -- c).updated(n, c).updated(m, c)
+    })
+  }
+
+  def rayHeuristics(puzzle: Puzzle): List[Puzzle => Puzzle] = {
+    val rowUpdaters: List[Puzzle => Puzzle] = puzzle.indices.map(i => {
+      val rowExtractor: Puzzle => Vector[Cell] = (p: Puzzle) => p.row(i)
+      val rowUpdater: (Puzzle, Vector[Cell]) => Puzzle = (p: Puzzle, row: Vector[Cell]) => p.withRow(i)(row)
+      rayHeuristic(rowExtractor, rowUpdater)
+    }).toList
+    val colUpdaters: List[Puzzle => Puzzle] = puzzle.indices.map(i => {
+      val colExtractor: Puzzle => Vector[Cell] = (p: Puzzle) => p.col(i)
+      val colUpdater: (Puzzle, Vector[Cell]) => Puzzle = (p: Puzzle, col: Vector[Cell]) => p.withCol(i)(col)
+      rayHeuristic(colExtractor, colUpdater)
+    }).toList
+    rowUpdaters ::: colUpdaters
+  }
+
+  case class IdentityConstraint(puzzle1Index: Int, puzzle1Location: Location, puzzle2Index: Int, puzzle2Location: Location)
+
+  case class CompositePuzzle(puzzles: Vector[Puzzle], identityConstraints: Set[IdentityConstraint])
+
+  def identityHeuristic(constraint: IdentityConstraint): CompositePuzzle => CompositePuzzle = {
+    val i1 = constraint.puzzle1Index
+    val i2 = constraint.puzzle2Index
+    val loc1 = constraint.puzzle1Location
+    val loc2 = constraint.puzzle2Location
+
+    cp: CompositePuzzle => {
+      val puzzles = cp.puzzles
+      val cell1 = puzzles(i1).cellAt(loc1)
+      val cell2 = puzzles(i2).cellAt(loc2)
+      val cell: Cell = cell1 intersect cell2
+      val puzzle1 = puzzles(i1).withCell(loc1, cell)
+      val puzzle2 = puzzles(i2).withCell(loc2, cell)
+      val updatedPuzzles = puzzles.updated(i1, puzzle1).updated(i2, puzzle2)
+      CompositePuzzle(updatedPuzzles, cp.identityConstraints)
+    }
+  }
+
+  def subPuzzleHeuristic: CompositePuzzle => CompositePuzzle = c => {
+    val solutions: Vector[Puzzle] = c.puzzles.map(p => {
+      val heuristics = crossHeuristic :: pigeonholeHeuristic :: rayHeuristics(p) :::
+        (p.numericConstraints map numHeuristic).toList :::
+        (p.ltConstraints map ltHeuristic).toList
+      new Solver[Puzzle](p,
+        heuristics ::: List(contradictionHeuristic(heuristics))
+      ).solution
+    })
+    CompositePuzzle(solutions, c.identityConstraints)
+  }
+
+  /**
+   * @param speculate gives the speculative form of a puzzle
+   * @param reject gives the form of the puzzle if the speculation must be rejected
+   */
+  case class Dual(speculate: Puzzle => Puzzle, reject: Puzzle => Puzzle)
+
+  def contradiction(p: Puzzle): Boolean = p.state.flatten.exists(cell => cell.size < 1)
+
+  def solve(p: Puzzle, hs: List[Puzzle => Puzzle]): Puzzle = new Solver(p, hs).solution
+
+  def hypotheticalFails(hypothetical: Puzzle, hs: List[Puzzle => Puzzle]): Boolean = {
+    Try(contradiction(solve(hypothetical, hs))) match {
+      case Success(b) => b
+      case Failure(_) => true
+    }
+  }
+
+  // TODO: Contradiction heuristic. Take a puzzle, assume a direction as an answer, solve, and if the solution contains
+  // a contradiction (cell with an empty set), remove the hypothesized value from consideration. Otherwise, don't.
+  // IMPORTANT: Don't include the contradiction heuristic when checking the feasibility of a contradiction heuristic!
+  def contradictionHeuristic(hs: List[Puzzle => Puzzle]): Puzzle => Puzzle = p => {
+    // Out of the possible pairs of speculation duals and resulting solved puzzle states based on given heuristics,
+    val onePlyDuals = for {
+      rowIndex <- p.indices
+      colIndex <- p.indices
+      // if p.state(rowIndex)(colIndex).size == 2
+      if p.state(rowIndex)(colIndex).size > 1
+      value <- p.state(rowIndex)(colIndex)
+    } yield Dual(z => z.set(Location(rowIndex, colIndex), value), z => z.clear(Location(rowIndex, colIndex), value))
+
+    // for the resulting states which are contradictory,
+    val rejectors = for {
+      dual <- onePlyDuals
+      hypothetical = dual.speculate(p)
+      if hypotheticalFails(hypothetical, hs)
+    } yield dual.reject
+
+    // the resulting puzzle should not consider those values
+    rejectors.foldLeft(p)((z: Puzzle, reject: Puzzle => Puzzle) => reject(z))
   }
 }
--- solver/src/test/scala/github/johngorskijr/aenigmata/adas/SuperSudokuTest.scala
+++ solver/src/test/scala/github/johngorskijr/aenigmata/adas/SuperSudokuTest.scala
@@ -37,11 +37,176 @@ class SuperSudokuTest extends FunSuite {
       ) ::: (sampleLtConstraints map ltHeuristic).toList
     )
 
-    // println(solver.solution)
-    solver.steps.foreach(p => println("-----\n" + p))
+    println(solver.solution)
+    // solver.steps.foreach(p => println("-----\n" + p))
   }
 
+  test("updateRay normalizes double pair") {
+    val pair    = Set(   2,          6)
+    val removed = Set(1,    3, 4, 5,    7)
+    val cell = pair union removed
+
+    val ray      = Vector(cell,    pair, cell,    pair, cell,    cell,    cell)
+    val expected = Vector(removed, pair, removed, pair, removed, removed, removed)
+
+    assert(updateRay(ray) === expected)
+  }
+
+  // TODO: triple triplets heuristic/n n-tuples if needed
+
   test("solve main") {
-    ???
+    val p7 = freshPuzzle(7)
+
+    val aNumConstraints = Set(
+      NumConstraint((Location(0, 2), Location(1, 2)), 4),
+      NumConstraint((Location(1, 0), Location(2, 0)), 2),
+      NumConstraint((Location(2, 2), Location(3, 2)), 4),
+      NumConstraint((Location(2, 4), Location(3, 4)), 3),
+      NumConstraint((Location(3, 4), Location(4, 4)), 3)
+    )
+
+    val aLtConstraints = Set(
+      LtConstraint(Location(0, 0), Location(0, 1)),
+      LtConstraint(Location(0, 1), Location(1, 1)),
+      LtConstraint(Location(1, 1), Location(1, 0)),
+      LtConstraint(Location(1, 2), Location(1, 3)),
+      LtConstraint(Location(1, 3), Location(1, 4)),
+      LtConstraint(Location(2, 6), Location(2, 5)),
+      LtConstraint(Location(2, 5), Location(3, 5)),
+      LtConstraint(Location(3, 5), Location(4, 5)),
+      LtConstraint(Location(5, 2), Location(4, 2)),
+      LtConstraint(Location(5, 0), Location(6, 0)),
+      LtConstraint(Location(6, 2), Location(6, 1)),
+      LtConstraint(Location(5, 3), Location(6, 3)),
+      LtConstraint(Location(6, 6), Location(5, 6))
+    )
+
+    val puzzleA = Puzzle(p7, aNumConstraints, aLtConstraints)
+
+    val bNumConstraints = Set(
+      NumConstraint((Location(0, 5), Location(0, 6)), 4),
+      NumConstraint((Location(0, 4), Location(1, 4)), 6),
+      NumConstraint((Location(2, 5), Location(3, 5)), 4),
+      NumConstraint((Location(3, 3), Location(4, 3)), 6),
+      NumConstraint((Location(4, 1), Location(4, 2)), 2),
+      NumConstraint((Location(5, 1), Location(5, 2)), 5),
+      NumConstraint((Location(5, 5), Location(5, 6)), 2)
+    )
+
+    val bLtConstraints = Set(
+      LtConstraint(Location(0, 2), Location(0, 3)),
+      LtConstraint(Location(1, 2), Location(0, 2)),
+      LtConstraint(Location(2, 2), Location(1, 2)),
+      LtConstraint(Location(2, 4), Location(2, 3)),
+      LtConstraint(Location(6, 2), Location(6, 3))
+    )
+
+    val puzzleB = Puzzle(p7, bNumConstraints, bLtConstraints)
+
+    val cNumConstraints = Set(
+      NumConstraint((Location(1, 0), Location(2, 0)), 4),
+      NumConstraint((Location(1, 6), Location(2, 6)), 3),
+      NumConstraint((Location(2, 5), Location(3, 5)), 2),
+      NumConstraint((Location(3, 5), Location(4, 5)), 3),
+      NumConstraint((Location(4, 5), Location(5, 5)), 4),
+      NumConstraint((Location(5, 1), Location(5, 2)), 4),
+      NumConstraint((Location(5, 0), Location(6, 0)), 6)
+    )
+
+    val cLtConstraints = Set(
+      LtConstraint(Location(0, 1), Location(0, 0)),
+      LtConstraint(Location(0, 2), Location(0, 1)),
+      LtConstraint(Location(0, 3), Location(0, 2)),
+      LtConstraint(Location(0, 3), Location(0, 4)),
+      LtConstraint(Location(0, 4), Location(0, 5)),
+      LtConstraint(Location(0, 5), Location(0, 6)),
+      LtConstraint(Location(2, 3), Location(2, 2)),
+      LtConstraint(Location(2, 2), Location(3, 2)),
+      LtConstraint(Location(3, 2), Location(3, 3)),
+      LtConstraint(Location(5, 2), Location(5, 3))
+    )
+
+    val puzzleC = Puzzle(p7, cNumConstraints, cLtConstraints)
+
+    val dNumConstraints = Set(
+      NumConstraint((Location(1, 5), Location(2, 5)), 4),
+      NumConstraint((Location(3, 2), Location(4, 2)), 2),
+      NumConstraint((Location(4, 6), Location(5, 6)), 2),
+      NumConstraint((Location(5, 3), Location(5, 4)), 1),
+      NumConstraint((Location(5, 0), Location(6, 0)), 3),
+      NumConstraint((Location(6, 5), Location(6, 6)), 6)
+    )
+
+    val dLtConstraints = Set(
+      LtConstraint(Location(1, 3), Location(0, 3)),
+      LtConstraint(Location(2, 1), Location(2, 0)),
+      LtConstraint(Location(2, 2), Location(2, 1)),
+      LtConstraint(Location(2, 3), Location(3, 3)),
+      LtConstraint(Location(3, 4), Location(2, 4)),
+      LtConstraint(Location(3, 3), Location(3, 4)),
+      LtConstraint(Location(4, 0), Location(3, 0)),
+      LtConstraint(Location(3, 5), Location(4, 5)),
+      LtConstraint(Location(5, 5), Location(4, 5)),
+      LtConstraint(Location(5, 2), Location(6, 2))
+    )
+
+    val puzzleD = Puzzle(p7, dNumConstraints, dLtConstraints)
+
+    val eNumConstraints = Set(
+      NumConstraint((Location(1, 5), Location(1, 6)), 3),
+      NumConstraint((Location(2, 3), Location(3, 3)), 1),
+      NumConstraint((Location(3, 6), Location(4, 6)), 3),
+      NumConstraint((Location(4, 2), Location(4, 3)), 4),
+      NumConstraint((Location(4, 3), Location(4, 4)), 2),
+      NumConstraint((Location(4, 4), Location(4, 5)), 3),
+      NumConstraint((Location(4, 1), Location(5, 1)), 6),
+      NumConstraint((Location(6, 3), Location(6, 4)), 4),
+      NumConstraint((Location(6, 5), Location(6, 6)), 5)
+    )
+
+    val eLtConstraints = Set(
+      LtConstraint(Location(1, 1), Location(0, 1)),
+      LtConstraint(Location(0, 4), Location(1, 4)),
+      LtConstraint(Location(1, 0), Location(1, 1)),
+      LtConstraint(Location(1, 2), Location(1, 1)),
+      LtConstraint(Location(1, 1), Location(2, 1)),
+      LtConstraint(Location(2, 4), Location(2, 3)),
+      LtConstraint(Location(2, 0), Location(3, 0))
+    )
+
+    val puzzleE = Puzzle(p7, eNumConstraints, eLtConstraints)
+
+    val identityConstraints = Set(
+      IdentityConstraint(0, Location(5, 4), 1, Location(0, 0)),
+      IdentityConstraint(0, Location(5, 5), 1, Location(0, 1)),
+      IdentityConstraint(0, Location(5, 6), 1, Location(0, 2)),
+      IdentityConstraint(0, Location(6, 4), 1, Location(1, 0)),
+      IdentityConstraint(0, Location(6, 5), 1, Location(1, 1)),
+      IdentityConstraint(0, Location(6, 6), 1, Location(1, 2)),
+      IdentityConstraint(1, Location(0, 4), 2, Location(5, 0)),
+      IdentityConstraint(1, Location(0, 5), 2, Location(5, 1)),
+      IdentityConstraint(1, Location(0, 6), 2, Location(5, 2)),
+      IdentityConstraint(1, Location(1, 4), 2, Location(6, 0)),
+      IdentityConstraint(1, Location(1, 5), 2, Location(6, 1)),
+      IdentityConstraint(1, Location(1, 6), 2, Location(6, 2)),
+      IdentityConstraint(2, Location(5, 4), 3, Location(0, 0)),
+      IdentityConstraint(2, Location(5, 5), 3, Location(0, 1)),
+      IdentityConstraint(2, Location(5, 6), 3, Location(0, 2)),
+      IdentityConstraint(2, Location(6, 4), 3, Location(1, 0)),
+      IdentityConstraint(2, Location(6, 5), 3, Location(1, 1)),
+      IdentityConstraint(2, Location(6, 6), 3, Location(1, 2)),
+      IdentityConstraint(3, Location(0, 4), 4, Location(5, 0)),
+      IdentityConstraint(3, Location(0, 5), 4, Location(5, 1)),
+      IdentityConstraint(3, Location(0, 6), 4, Location(5, 2)),
+      IdentityConstraint(3, Location(1, 4), 4, Location(6, 0)),
+      IdentityConstraint(3, Location(1, 5), 4, Location(6, 1)),
+      IdentityConstraint(3, Location(1, 6), 4, Location(6, 2))
+    )
+
+    val puzzle = CompositePuzzle(Vector(puzzleA, puzzleB, puzzleC, puzzleD, puzzleE), identityConstraints)
+
+    val solver = new Solver[CompositePuzzle](puzzle, subPuzzleHeuristic :: (identityConstraints map identityHeuristic).toList)
+
+    println(solver.solution)
   }
 }
